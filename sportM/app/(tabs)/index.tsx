1824e2982c2b2607a902430748b53ba054d8021c
Luongnb3213 <luongnb3213@gmail.com>
2025-08-24 20:19:20 +0700

    feat: Configure Firebase and update Android build settings

--- sportM/app/(tabs)/index.tsx
+++ sportM/app/(tabs)/index.tsx
@@ -1,46 +1,66 @@
 import { Button, StyleSheet, Text, View } from 'react-native';
 import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
 import React, { useState, useCallback, useEffect } from 'react';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import { signInWithCredential, GoogleAuthProvider } from 'firebase/auth';
-import { auth } from '../../firebaseConfig';
+import {
+  GoogleSignin,
+  isSuccessResponse,
+  isErrorWithCode,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 
 GoogleSignin.configure({
   webClientId:
-    '1013531353218-ecdkpttpkjdah5a4bi40m942s4ljpmak.apps.googleusercontent.com',
+    '504083896204-iff4f78io6sc5rs1otq0t9o1lhitignv.apps.googleusercontent.com',
+  profileImageSize: 120,
+  iosClientId:
+    '504083896204-du75dra9lbe1kglvlsrsa5apv7d3145e.apps.googleusercontent.com',
 });
 
 export default function HomeScreen() {
-  const signInWithGoogle = async () => {
+
+    const signOutFromGoogle = async () => {
     try {
-      await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signIn();
-
-      const { idToken } = await GoogleSignin.getTokens(); // Lấy token rõ ràng
-
-      const googleCredential = GoogleAuthProvider.credential(idToken);
-      const userCredential = await signInWithCredential(auth, googleCredential);
-
-      console.log('Logged in as:', userCredential.user);
+      await GoogleSignin.signOut();
+      console.log("User signed out!");
     } catch (error) {
-      console.error(error);
+      console.error("Sign out error:", error);
     }
   };
-
-  const signOutFromGoogle = async () => {
-  try {
-    await GoogleSignin.signOut();
-    console.log("User signed out!");
-  } catch (error) {
-    console.error("Sign out error:", error);
-  }
-};
 
   const width = useSharedValue(100);
 
   const handlePress = () => {
     width.value = withSpring(width.value + 50);
   };
+   
+  const handleGoogleSignin = async () => {
+     try {
+      await GoogleSignin.hasPlayServices();
+      const response = await GoogleSignin.signIn();
+      if(isSuccessResponse(response)) {
+        const { idToken, user } = response.data;
+        const { name, email, photo } = user;
+        console.log("User Info:", { idToken, name, email, photo });
+      }
+     } catch (error) {
+        if(isErrorWithCode(error)) {
+          switch(error.code) {
+            case statusCodes.SIGN_IN_CANCELLED:
+              console.log("User cancelled the login flow");
+              break;
+            case statusCodes.IN_PROGRESS:
+              console.log("Sign in is in progress already");
+              break;
+            case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+              console.log("Play services not available or outdated");
+              break;
+            default:
+              console.log("Some other error happened:", error);
+          }
+        }
+     }
+  }
+
   return (
     <View className="flex-1 justify-center items-center">
       <Text>Edit app/index.tsx to edit this screen</Text>
@@ -51,9 +71,8 @@ export default function HomeScreen() {
           backgroundColor: 'violet',
         }}
       />
-      <Button onPress={signInWithGoogle} title="Click gg" />
+      <Button onPress={handleGoogleSignin} title="Click gg" />
       <Button onPress={signOutFromGoogle} title="Sign out" />
-
     </View>
   );
 }
